97949625127c9af05c1a600626f99627965f77a8
Suissa <jnascimento@gmail.com>
2015-07-23 22:57:49 -0300

    View para edição da cerveja

--- modules/beers/routes/index.js
+++ modules/beers/routes/index.js
@@ -4,5 +4,6 @@ var Controller = require('./../controller');
 
 router.get('/', Controller.renderList);
 router.get('/:id', Controller.renderGet);
+router.get('/:id/edit', Controller.renderGet);
 
 module.exports = router;
